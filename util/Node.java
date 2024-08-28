01d35ac2058e21be53973df3aa64fa7606df07f9
Nicolas Henriques de Almeida <Nhenriquesdealmeida@gmail.com>
2024-08-28 18:17:17 -0300

    Adding getters and setters to the Node class

--- util/Node.java
+++ util/Node.java
@@ -10,30 +10,68 @@ public class Node<T> {
 	
 	//constructor methods
 	public Node() {
-		this.data = null;
-		this.parent = null;
-		this.left = null;
-		this.right = null;
+		this.setData(null);
+		this.setParent(null);
+		this.setLeft(null);
+		this.setRight(null);
 	}
 	
 	public Node(T data) {
-		this.data = data;
-		this.parent = null;
-		this.left = null;
-		this.right = null;
+		this.setData(data);
+		this.setParent(null);
+		this.setLeft(null);
+		this.setRight(null);
 	}
 	
 	public Node(T data, Node<T> parent) {
-		this.data = data;
-		this.parent = parent;
-		this.left = null;
-		this.right = null;
+		this.setData(data);
+		this.setParent(parent);
+		this.setLeft(null);
+		this.setRight(null);
 	}
 	
 	public Node(T data, Node<T> parent, Node<T> left, Node<T> right) {
+		this.setData(data);
+		this.setParent(parent);
+		this.setLeft(left);
+		this.setRight(right);
+	}
+
+	//getters and setters methods
+	public T getData() {
+		return data;
+	}
+
+	public void setData(T data) {
 		this.data = data;
+	}
+
+	public Node<T> getParent() {
+		return parent;
+	}
+
+	public void setParent(Node<T> parent) {
 		this.parent = parent;
+	}
+
+	public Node<T> getLeft() {
+		return left;
+	}
+
+	public void setLeft(Node<T> left) {
 		this.left = left;
+	}
+
+	public Node<T> getRight() {
+		return right;
+	}
+
+	public void setRight(Node<T> right) {
 		this.right = right;
+	}
+	
+	//defining node methods
+	public boolean isRoot() {
+		return parent == null ? true : false;
 	}
 }
